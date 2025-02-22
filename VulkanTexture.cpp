7c14cd7ca88325438e3e96147c1c363af983e63e
Rich Davison <richard-gordon.davison@ncl.ac.uk>
2025-02-22 17:43:33 +0000

    Trying a rejigged mipmap chain, there's issues on old intel GPUs, where a few mips down the line, the image starts to get corrupted.

--- VulkanTexture.cpp
+++ VulkanTexture.cpp
@@ -31,23 +31,23 @@ VulkanTexture::~VulkanTexture() {
 void VulkanTexture::GenerateMipMaps(vk::CommandBuffer  buffer, vk::ImageLayout endLayout, vk::PipelineStageFlags2 endFlags) {
 	for (int layer = 0; layer < layerCount; ++layer) {	
 		ImageTransitionBarrier(buffer, image, 
-			vk::ImageLayout::eUndefined, vk::ImageLayout::eTransferSrcOptimal, 
+			vk::ImageLayout::eTransferDstOptimal, vk::ImageLayout::eTransferSrcOptimal, 
 			aspectType, 
 			vk::PipelineStageFlagBits2::eAllCommands, vk::PipelineStageFlagBits2::eTransfer, 
 			0, 1, layer, 1);
-		
-		for (uint32_t mip = 1; mip < mipCount; ++mip) {
+
 			vk::ImageBlit blitData;
 			blitData.srcSubresource.setAspectMask(vk::ImageAspectFlagBits::eColor)
-				.setMipLevel(mip - 1)
+				.setMipLevel(0)
 				.setBaseArrayLayer(layer)
 				.setLayerCount(1);
 
 			blitData.srcOffsets[0] = vk::Offset3D(0, 0, 0);
-			blitData.srcOffsets[1].x = std::max(dimensions.x >> (mip - 1), (uint32_t)1);
-			blitData.srcOffsets[1].y = std::max(dimensions.y >> (mip - 1), (uint32_t)1);
+			blitData.srcOffsets[1].x = dimensions.x;
+			blitData.srcOffsets[1].y = dimensions.y;
 			blitData.srcOffsets[1].z = 1;
-
+	
+		for (uint32_t mip = 1; mip < mipCount; ++mip) {
 			blitData.dstSubresource.setAspectMask(vk::ImageAspectFlagBits::eColor)
 				.setMipLevel(mip)
 				.setBaseArrayLayer(layer)
@@ -57,34 +57,30 @@ void VulkanTexture::GenerateMipMaps(vk::CommandBuffer  buffer, vk::ImageLayout e
 			blitData.dstOffsets[1].x = std::max(dimensions.x >> mip, (uint32_t)1);
 			blitData.dstOffsets[1].y = std::max(dimensions.y >> mip, (uint32_t)1);
 			blitData.dstOffsets[1].z = 1;
-
+			//Prepare the new mip level to receive the blitted data
 			ImageTransitionBarrier(buffer, image, 
 				vk::ImageLayout::eUndefined, vk::ImageLayout::eTransferDstOptimal, 
 				aspectType, 
-				vk::PipelineStageFlagBits2::eBlit, vk::PipelineStageFlagBits2::eBlit, 
+				vk::PipelineStageFlagBits2::eTransfer, vk::PipelineStageFlagBits2::eTransfer,
 				mip, 1, layer, 1);
 			
 			buffer.blitImage(image, vk::ImageLayout::eTransferSrcOptimal, image, vk::ImageLayout::eTransferDstOptimal, blitData, vk::Filter::eLinear);
-			ImageTransitionBarrier(buffer, image, 
-				vk::ImageLayout::eTransferSrcOptimal, endLayout, 
-				aspectType, 
-				vk::PipelineStageFlagBits2::eBlit, endFlags, 
-				mip - 1, 1, layer, 1);
+			//The new mip is then transitioned again to be able to act as a source
+			//for the next level 
+			ImageTransitionBarrier(buffer, image,
+				vk::ImageLayout::eTransferDstOptimal, vk::ImageLayout::eTransferSrcOptimal,
+				aspectType,
+				vk::PipelineStageFlagBits2::eTransfer, vk::PipelineStageFlagBits2::eTransfer,
+				mip, 1, layer, 1);
 
-			if (mip < this->mipCount - 1) {
-				ImageTransitionBarrier(buffer, image, 
-					vk::ImageLayout::eTransferDstOptimal, vk::ImageLayout::eTransferSrcOptimal, 
-					aspectType, 
-					vk::PipelineStageFlagBits2::eBlit, vk::PipelineStageFlagBits2::eBlit, 
-					mip, 1, layer, 1);
-			}
-			else {
-				ImageTransitionBarrier(buffer, image, 
-					vk::ImageLayout::eTransferDstOptimal, endLayout, 
+			blitData.srcOffsets		= blitData.dstOffsets;
+			blitData.srcSubresource = blitData.dstSubresource;
+		}
+		//Now that this layer's mipchain is complete, transition the whole lot to final layout
+			ImageTransitionBarrier(buffer, image, 
+					vk::ImageLayout::eTransferSrcOptimal, endLayout,
 					aspectType, 
 					vk::PipelineStageFlagBits2::eBlit, endFlags, 
-					mip, 1, layer, 1);
-			}
-		}
+					0, mipCount, layer, 1);
 	}
 }
